5f49eca8eed394d9188169fc2cbdf45bfb7f13c4
unknown <muhamadrizki261004@gmail.com>
2025-09-24 20:16:09 +0700

    Tugas P6 JS

---
+++ Pertemuan 6 - 24 Sep 2025/controller.js
@@ -0,0 +1,37 @@
+let data = require('./data.js');
+
+function tampil() {
+    console.log("=================== Daftar =====================");
+    data.map((item, index) => {
+        console.log(
+            `${index + 1}. Nama: ${item.nama}, Umur: ${item.umur}, Alamat: ${item.alamat}, Email: ${item.email}`
+        );
+    });
+}
+
+function tambah() {
+    data.push(
+        { nama: "Maya", umur: 21, alamat: "Bogor", email: "maya@mail.com" },
+        { nama: "Rian", umur: 22, alamat: "Depok", email: "rian@mail.com" }
+    );
+    console.log("\nData ditambahkan\n");
+    tampil();
+}
+
+// 3. Menghapus data berdasarkan index
+function hapus(index) {
+    if (index >= 0 && index < data.length) {
+        let hapus = data.splice(index, 1);
+        console.log(`\nData ${hapus[0].nama} dihapus`);
+    }
+    tampil();
+}
+
+console.log(">>> Awal Data");
+tampil();
+
+console.log("\n>>> Tambah Data");
+tambah();
+
+console.log("\n>>> Hapus Data (hapus index ke-2 / data ke-3)");
+hapus(2);
---
+++ Pertemuan 6 - 24 Sep 2025/data.js
@@ -0,0 +1,14 @@
+let data = [
+    { nama: "Muhamad Rizki Ramadhan Siregar", umur: 21, alamat: "Jakarta Utara", email: "muhamadrizki261004@mail.com" },
+    { nama: "Budi", umur: 22, alamat: "Depok", email: "budi@mail.com" },
+    { nama: "Siti", umur: 21, alamat: "Bekasi", email: "siti@mail.com" },
+    { nama: "Agus", umur: 23, alamat: "Bogor", email: "agus@mail.com" },
+    { nama: "Andi", umur: 19, alamat: "Tangerang", email: "andi@mail.com" },
+    { nama: "Dewi", umur: 24, alamat: "Bandung", email: "dewi@mail.com" },
+    { nama: "Nina", umur: 20, alamat: "Cimahi", email: "nina@mail.com" },
+    { nama: "Eko", umur: 22, alamat: "Cikarang", email: "eko@mail.com" },
+    { nama: "Tono", umur: 25, alamat: "Karawang", email: "tono@mail.com" },
+    { nama: "Lia", umur: 23, alamat: "Cileungsi", email: "lia@mail.com" }
+];
+
+module.exports = data;
